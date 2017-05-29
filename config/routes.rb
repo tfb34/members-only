2846d50ab9548d7039ccc63a6e66f5ebca8dd57e
tfb34 <britothalia212@gmail.com>
2017-05-28 22:33:55 -0400

    Make simple static pages (including a link to log in)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root 'static_pages#home'
+  
+  get '/home', to: 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+
+  get '/login', to: 'sessions#new'
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
